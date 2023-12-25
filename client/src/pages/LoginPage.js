88f515abb6b911305eadeebc7e4fa392f9a006cd
Fauzan <ojanaja03@gmail.com>
2023-12-25 18:45:17 +0700

    test login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
     const { setUserInfo } = useContext(UserContext);
     async function login(ev) {
         ev.preventDefault();
-        const response = await fetch('https://web-blog-zeta-seven.vercel.app/login', {
+        const response = await fetch('https://web-blog-uttw.vercel.app/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
